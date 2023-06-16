18ec96ee01a333c5310c795089072b5f3e67694e
Shubham Mahore <shubham.mahore@dnow.com>
2023-06-17 00:59:09 +0530

    Material Basic UI Components Hands-on

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import BasicTable from './BasicTable/BasicTable';
+import StickyHeadTable from './BasicTable/StickyHeadTable';
+import EnhancedTable from './BasicTable/EnhancedTable';
+import SimpleAccordion from './Accordion/SimpleAccordion';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import FreeSolo from './AutoComplete/FreeSolo';
+import Button from '@mui/material/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React App</h1>
+      <Box sx={{ width: '100%', maxWidth: 500 }}>
+      <Typography variant="h3" gutterBottom>
+      StickyHeadTable Example
+      </Typography>
+      </Box>
+      <FreeSolo></FreeSolo>
+      <Button variant="contained">Submit</Button>
+      <SimpleAccordion></SimpleAccordion>
+      {/* <EnhancedTable></EnhancedTable> */}
+      {/* <StickyHeadTable></StickyHeadTable> */}
+      {/* <BasicTable></BasicTable> */}
+      
     </div>
   );
 }
